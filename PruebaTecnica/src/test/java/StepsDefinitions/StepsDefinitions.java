ece13c1de0a23bc9aab974972cd84a7f213f01c7
William Steearing Cordoba Mosquera <mcsteeharing@gmail.com>
2019-08-18 23:21:37 -0500

    Ajustes finales.

--- PruebaTecnica/src/test/java/StepsDefinitions/StepsDefinitions.java
+++ PruebaTecnica/src/test/java/StepsDefinitions/StepsDefinitions.java
@@ -21,15 +21,14 @@ public class StepsDefinitions {
 	Configuracion configuracion = new Configuracion();
 	Utilidades utilidades = new Utilidades();
 
-	/*@Given("abro el navegador e ingreso al buscador de google")
+	@Given("abro el navegador e ingreso al buscador de google")
 	public void abro_el_navegador_e_ingreso_al_buscador_de_google() {
-		System.setProperty(configuracion.leerArchivo("webDriver").getMensaje(),
-				configuracion.leerArchivo("ruta.driver").getMensaje());
+		System.setProperty(configuracion.leerArchivo("webDriver").getMensaje(),configuracion.leerArchivo("ruta.driver").getMensaje());
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(configuracion.leerArchivo("url").getMensaje());
-	}*/
+	}
 
 	@When("dijito la palabra pruebaz")
 	public void dijito_la_palabra_pruebaz() throws Exception {
@@ -61,18 +60,6 @@ public class StepsDefinitions {
 	public void verifico_que_sean_mas_de() {
 		PageGoogle pageGoogle = PageFactory.initElements(driver, PageGoogle.class);
 		pageGoogle.verificarResultados(driver);
-	}
-	
-	
-	
-	@Given("^abro el navegador e ingreso al buscador de google$")
-	public void abro_el_navegador_e_ingreso_al_buscador_de_google() throws Throwable {
-		System.setProperty(configuracion.leerArchivo("webDriver").getMensaje(),
-				configuracion.leerArchivo("ruta.driver").getMensaje());
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		driver.get(configuracion.leerArchivo("url").getMensaje());
 	}
 
 }
